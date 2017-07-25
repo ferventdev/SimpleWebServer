57760548c81912dffb019ffb6756c95b427c758d
ferventdev <alex.4everyours@gmail.com>
2017-07-25 16:43:34 +0300

    ConnectionProcessor class created.

---
+++ src/main/java/http/ConnectionProcessor.java
@@ -0,0 +1,12 @@
+package http;
+
+/**
+ * Created by Aleksandr Shevkunenko on 25.07.2017.
+ */
+public class ConnectionProcessor implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
