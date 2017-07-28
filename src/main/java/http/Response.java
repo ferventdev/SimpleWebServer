e5add0068b6e920d667735f41d1f056c2b78d741
ferventdev <alex.4everyours@gmail.com>
2017-07-28 18:16:22 +0300

    Response interface updated.

--- src/main/java/http/Response.java
+++ src/main/java/http/Response.java
@@ -1,19 +1,41 @@
 package http;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Map;
+
 /**
  * Created by Aleksandr Shevkunenko on 26.07.2017.
  */
 public interface Response {
 
-    enum Status { OK }
+//    enum Status { OK }
 
-    Status getStatus();
+    String getHttpVersion();
+    String getStatus();
+    Map<String, String> getHeaders();
+    InputStream getBody();
 
-    static Response from(Status status) {
+    static Response from(String httpVersion, String status, Map<String, String> headers, InputStream body) {
         return new Response() {
             @Override
-            public Status getStatus() {
+            public String getHttpVersion() {
+                return httpVersion;
+            }
+
+            @Override
+            public String getStatus() {
                 return status;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() {
+                return headers;
+            }
+
+            @Override
+            public InputStream getBody() {
+                return body;
             }
         };
     }
